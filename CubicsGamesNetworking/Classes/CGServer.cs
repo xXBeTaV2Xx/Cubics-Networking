8302e4c46ec4b20ef0a0d5924cc8b38f94bfbfd4
xXBeTaV2Xx <31585514+xXBeTaV2Xx@users.noreply.github.com>
2017-09-03 10:25:12 +0200

    Added Fieles via Upload
    TestUpload

---
+++ CubicsGamesNetworking/Classes/CGServer.cs
@@ -0,0 +1,248 @@
+﻿using CubicsGamesNetworking.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace CubicsGamesNetworking
+{
+    public class CGServer
+    {
+        private TcpListener ClientsServer;
+        private Thread WorkThread;
+        private Thread ClientsListenThread;
+        public delegate void OnConnectEvent(Connection con);
+        public delegate void OnDefaultMSGEvent(Connection con,string msg);
+        public delegate void OnDisconnectEvent(Connection con);
+        public delegate void OnMSGEvent(Connection con,string msgcontent);
+        private Dictionary<ulong, Connection> Clients = new Dictionary<ulong, Connection>();
+        public Dictionary<string, OnMSGEvent> OnMSGFilters = new Dictionary<string, OnMSGEvent>();
+        public OnDisconnectEvent OnClientDisconnected;
+        public OnConnectEvent OnClientConnected;
+        public OnDefaultMSGEvent OnDefaultMSG;
+        private UInt64 LastID = 0;
+
+        private bool CallWorkExternal = false;
+
+        private bool Running = false;
+
+        public bool IsUseExternalWorkCall()
+        {
+            return CallWorkExternal;
+        }
+
+        public void SetExternalWorkCallUsage(bool state)
+        {
+            if (!IsRunning())
+            {
+                CallWorkExternal = state;
+            }
+        }
+
+        public bool IsRunning()
+        {
+            return Running;
+        }
+
+        public void StartServer(int Port)
+        {
+            if (Running)
+            {
+                return;
+            }
+            try
+            {
+                ClientsServer = new TcpListener(IPAddress.Any, Port);
+                ClientsServer.Start();
+                Running = true;
+                ClientsListenThread = new Thread(ClientsListen);
+                ClientsListenThread.Start();
+                if (!IsUseExternalWorkCall())
+                {
+                    WorkThread = new Thread(Work);
+                    WorkThread.Start();
+                }
+            }
+            catch (Exception e){ StopServer(); }
+        }
+
+        public void StopServer()
+        {
+            try { if (ClientsListenThread != null) { if (ClientsListenThread.IsAlive) { ClientsListenThread.Abort(); } ClientsListenThread = null; } } catch (Exception e) { }
+            try { if (WorkThread != null) { if (WorkThread.IsAlive) { WorkThread.Abort(); } WorkThread = null; } } catch (Exception e) { }
+            try { if (ClientsServer != null) { ClientsServer.Stop(); ClientsServer = null; } } catch (Exception e) { }
+            KickAll();
+            Running = false;
+        }
+
+        private void Work()
+        {
+            try
+            {
+                while (IsRunning())
+                {
+                    try
+                    {
+                        DoWork(true);
+                    }
+                    catch (Exception e) { }
+                }
+            }
+            catch (Exception ee)
+            {
+
+            }
+        }
+
+        public void DoWork(bool WaitIfNothingIsToDo)
+        {
+            for (int i = 0; i < Clients.Keys.Count; i++)
+            {
+                ulong ClientKey = Clients.ElementAt(i).Key;
+                if (Clients[ClientKey].IsDisconnected())
+                {
+                    if (OnClientDisconnected != null)
+                    {
+                        OnClientDisconnected(Clients[ClientKey]);
+                    }
+                    Clients[ClientKey].Kick();
+                    Clients.Remove(ClientKey);
+                    i--;
+                }
+                else
+                {
+                    try
+                    {
+                        while (IsRunning())
+                        {
+                            string msg = Clients[ClientKey].GetMSG();
+                            if (msg != "" && msg != string.Empty)
+                            {
+                                ProcessMSG(Clients[ClientKey], msg);
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    catch (Exception e) { }
+                }
+            }
+            if (Clients.Keys.Count == 0 && WaitIfNothingIsToDo)
+            {
+                Thread.Sleep(5);
+            }
+        }
+
+        private void ProcessMSG(Connection con,string msg)
+        {
+            string PacketName = "";
+            string PacketContent = "";
+
+            if(msg != string.Empty)
+            {
+                string[] vals = msg.Split(' ');
+                if(vals.Length != 2)
+                {
+                    return;
+                }
+                PacketName = vals[0].Replace("!","");
+                PacketContent = vals[1];
+                if (OnMSGFilters.ContainsKey(PacketName))
+                {
+                    OnMSGFilters[PacketName](con, PacketContent);
+                }
+                else
+                {
+                    if(OnDefaultMSG != null)
+                    {
+                        OnDefaultMSG(con, msg);
+                    }
+                }
+            }
+        }
+
+        private void ClientsListen()
+        {
+            try
+            {
+                while (IsRunning())
+                {
+                    try
+                    {
+                        TcpClient client = ClientsServer.AcceptTcpClient();
+                        Connection con = new Connection(client, LastID);
+                        LastID++;
+                        if (OnClientConnected != null)
+                        {
+                            OnClientConnected(con);
+                        }
+                        Clients.Add(con.getUID(), con);
+                    }
+                    catch (Exception e) { }
+                }
+            }
+            catch (Exception exception)
+            {
+                /* Clean up. */
+            }
+        }
+
+        public void KickAll()
+        {
+            try
+            {
+                for (int i = 0; i < Clients.Keys.Count; i++)
+                {
+                    ulong ClientKey = Clients.ElementAt(i).Key;
+                    Clients[ClientKey].Kick();
+                    Clients.Remove(ClientKey);
+                    i--;
+                }
+            } catch (Exception e){ }
+        }
+
+        public void Kick(UInt64 id)
+        {
+            try
+            {
+                for (int i = 0; i < Clients.Keys.Count; i++)
+                {
+                    ulong ClientKey = Clients.ElementAt(i).Key;
+                    if (Clients[ClientKey].getUID() == id)
+                    {
+                        Clients[ClientKey].Kick();
+                        i = Clients.Count;
+                    }
+                }
+            }
+            catch (Exception e) { }
+        }
+
+        public  void Kick(Connection client)
+        {
+            try
+            {
+                client.Kick();
+            }
+            catch (Exception e) { }
+        }
+
+        public Connection GetConnection(ulong id)
+        {
+            if (Clients.ContainsKey(id))
+            {
+                return Clients[id];
+            }
+            return null;
+        }
+
+        public List<Connection> GetAllConnections()
+        {
+            return Clients.Values.ToList<Connection>();
+        }
+    }
+}
---
+++ CubicsGamesNetworking/Tools/Crypto.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CubicsGamesNetworking.Tools
+{
+    public static class Crypto
+    {
+        public static string DecryptXOR(string encripTedString, string passPhrase)
+        {
+            return EncryptXOR(encripTedString, passPhrase);
+        }
+
+        public static string EncryptXOR(string stringToEncript, string passPhrase)
+        {
+            var encriptedStringBuilder = new StringBuilder(stringToEncript.Length);
+            int positionInPassword = 0;
+
+            for (int i = 0; i < stringToEncript.Length; i++)
+            {
+                __corectPositionInPassWord(ref positionInPassword, passPhrase);
+                encriptedStringBuilder.Append((char)((int)stringToEncript[i] ^ (int)passPhrase[positionInPassword]));
+                ++positionInPassword;
+            }
+            return encriptedStringBuilder.ToString();
+        }
+
+        public static string EncryptSHA1(string text)
+        {
+            var sha1 = new SHA1CryptoServiceProvider();
+
+            string result = null;
+
+            var arrayData = Encoding.ASCII.GetBytes(text);
+            var arrayResult = sha1.ComputeHash(arrayData);
+            foreach (var t in arrayResult)
+            {
+                var temp = Convert.ToString(t, 16);
+                if (temp.Length == 1)
+                    temp = string.Format("0{0}", temp);
+                result += temp;
+            }
+            return result;
+        }
+
+        private static void __corectPositionInPassWord(ref int positionInPassword, string passPhrase)
+        {
+            if (positionInPassword == passPhrase.Length)
+            {
+                positionInPassword = 0;
+            }
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        public static string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = Convert.FromBase64String(base64EncodedData);
+            return Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
